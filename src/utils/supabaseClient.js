e30cc8c3f6cbf57ae06c0757fdaa85b699e3c6ac
Oktonius Zevanya <oktoniuszevanya@gmail.com>
2025-04-21 12:30:59 +0700

    fix(build): address file permission errors during compilation

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error(
+    "Supabase URL and Key must be defined in environment variables"
+  );
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
